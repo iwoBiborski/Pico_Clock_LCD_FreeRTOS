5fdad681dfa12019d5666e26e7cf15762d354cb0
Iwo Biborski <iwo.biborski@aptiv.com>
2022-07-11 07:52:44 -0700

    Chane of directories

--- Pico_Lcd_Clock/c/build/Pico_Clock/CMakeFiles/Pico_Clock.dir/ImageData.c.obj.d
+++
@@ -1,3 +0,0 @@
-Pico_Clock/CMakeFiles/Pico_Clock.dir/ImageData.c.obj: \
- /home/iwo/clock/Pico_Lcd_Clock/c/Pico_Clock/ImageData.c \
- /home/iwo/clock/Pico_Lcd_Clock/c/Pico_Clock/ImageData.h
--- Pico_Lcd_Clock/c/build/lib/Fonts/CMakeFiles/Fonts.dir/font24CN.c.obj.d
+++
@@ -1,8 +0,0 @@
-lib/Fonts/CMakeFiles/Fonts.dir/font24CN.c.obj: \
- /home/iwo/clock/Pico_Lcd_Clock/c/lib/Fonts/font24CN.c \
- /home/iwo/clock/Pico_Lcd_Clock/c/lib/Fonts/fonts.h \
- /usr/lib/gcc/arm-none-eabi/6.3.1/include/stdint.h \
- /usr/include/newlib/stdint.h \
- /usr/include/newlib/machine/_default_types.h \
- /usr/include/newlib/sys/features.h /usr/include/newlib/_newlib_version.h \
- /usr/include/newlib/sys/_intsup.h /usr/include/newlib/sys/_stdint.h
